d626ccde7cf531a72cff18af88181180a307f185
asd0905 <dydcodydco@daum.net>
2024-04-07 04:35:15 +0900

    메인 지도 수정

--- components/map.tsx
+++ components/map.tsx
@@ -1,122 +1,201 @@
 "use client";
 import { KAKAO_KEY } from "@/app/constants";
 import Script from "next/script";
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Skeleton } from "./ui/skeleton";
 declare const kakao: any;
+declare const window: any;
 
 export default function Map() {
 	const mapEl = useRef(null);
+	const [loading, setLoading] = useState(true);
+	const [kakaoLoad, setKakaoLoad] = useState(false);
 
-	const setKaKaoMap = async () => {
+	// 커스텀 오버레이를 닫기 위해 호출되는 함수입니다
+	window.closeOverlay = (overlay: any) => {
+		overlay.setMap(null);
+	};
+
+	// 지도에 마커를 표시하는 함수입니다
+	const addMarker = (place: any, clusterer: any) => {
+		// 마커를 생성하고 지도에 표시합니다
+		const marker = new kakao.maps.Marker({
+			position: new kakao.maps.LatLng(place.y, place.x),
+		});
+		clusterer.addMarker(marker);
+	};
+
+	const setMapLayOut = (selectedCafe: any, map: any, marker: any) => {
+		// 	const content = `
+		// <div class="bg-white border border-slate-500">
+		//     <div class="info">
+		//         <div class="grid grid-cols-2">
+		//             ${selectedCafe.place_name}
+		//             <div class="close" onclick="closeOverlay(overlay)" title="닫기">닫기</div>
+		//         </div>
+		//         <div class="flex">
+		//             <div class="felx flex-grow-0 flex-shrink-0 basis-auto">
+		//                 <img class='' src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/thumnail.png" width="73" height="70">
+		//             </div>
+		//             <div class="desc">
+		//                 <div class="ellipsis">${selectedCafe.address_name}</div>
+		//                 <div class="ellipsis">${selectedCafe.phone}</div>
+		//                 <div><a href="${selectedCafe.place_url}" target="_blank" class="link">홈페이지</a></div>
+		//             </div>
+		//         </div>
+		//     </div>
+		// </div>`;
+		// 	// 마커 위에 커스텀오버레이를 표시합니다
+		// 	// 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
+		// 	window.overlay = new kakao.maps.CustomOverlay({
+		// 		content: content,
+		// 		map: map,
+		// 		position: marker.getPosition(),
+		// 	});
+		// 	window.overlay.setMap(map);
+
+		// 인포윈도우로 장소에 대한 설명을 표시합니다
+		var infowindow = new kakao.maps.InfoWindow({
+			content: `<div style="width:150px;text-align:center;padding:6px 0;">${selectedCafe.place_name}</div>`,
+		});
+		infowindow.open(map, marker);
+	};
+
+	const setKaKaoMap = useCallback(async () => {
 		// HTML5의 geolocation으로 사용할 수 있는지 확인합니다
 		let lat = 0;
 		let lon = 0;
 		if (navigator.geolocation) {
 			// GeoLocation을 이용해서 접속 위치를 얻어옵니다
 			navigator.geolocation.getCurrentPosition((position) => {
-				console.log("position", position);
 				// lat = position.coords.latitude; // 위도
 				// lon = position.coords.longitude; // 경도
 				lon = 127.071315; // 위도
 				lat = 37.546542; // 경도
-
-				const locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-
-				// console.log("locPosition", locPosition);
-				// 마커와 인포윈도우를 표시합니다
-				// displayMarker(locPosition);
 
 				const infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
 				const container = mapEl.current; //지도를 담을 영역의 DOM 레퍼런스
 				const options = {
 					//지도를 생성할 때 필요한 기본 옵션
 					center: new kakao.maps.LatLng(lat, lon), //지도의 중심좌표.
-					level: 3, //지도의 레벨(확대, 축소 정도)
+					level: 4, //지도의 레벨(확대, 축소 정도)
 				};
 
 				// 지도를 생성합니다
 				const map = new kakao.maps.Map(container, options);
+				kakao.maps.event.addListener(map, "tilesloaded", function (data: any) {
+					setLoading(false);
+				});
+
+				const clusterer = new kakao.maps.MarkerClusterer({
+					map: map,
+					averageCenter: true,
+					minLevel: 3,
+				});
+
 				const imageSrc =
 					"//t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 				const imageSize = new kakao.maps.Size(24, 35);
 				const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-				new kakao.maps.Marker({
-					map: map,
-					position: new kakao.maps.LatLng(lat, lon),
-					image: markerImage, // 마커 이미지
-				});
+				const marker = new kakao.maps.Marker();
+				marker.setPosition(new kakao.maps.LatLng(lat, lon));
+				marker.setMap(map);
+				marker.setImage(markerImage);
+				clusterer.addMarkers(marker);
 
 				// 장소 검색 객체를 생성합니다
 				const ps = new kakao.maps.services.Places(map);
-				console.log("ps", ps);
-
-				// 카테고리로 은행을 검색합니다
-				ps.categorySearch("CE7", placesSearchCB, {
-					useMapBounds: true,
-				});
 
 				// 키워드 검색 완료 시 호출되는 콜백함수 입니다
-				async function placesSearchCB(data: any, status: any, pagination: any) {
+				const placesSearch = async (data: any, status: any, options: any) => {
 					if (status === kakao.maps.services.Status.OK) {
 						console.log(data);
-						const randomeIndex = Math.floor(Math.random() * data.length) + 1;
-						const selectedCafe = data.splice(randomeIndex, 1);
+						const randomeIndex = Math.floor(Math.random() * data.length);
+						const selectedCafe = data.splice(randomeIndex, 1)[0];
 						console.log("selectedCafe", selectedCafe);
-						// const reseponse = await fetch(
-						// 	`//place.map.kakao.com/m/main/v/${selectedCafe[0].id}`,
-						// 	{
-						// 		method: "GET",
-						// 		headers: {
-						// 			"Access-Control-Allow-Origin": "*",
-						// 		},
-						// 	}
-						// ).then((d) => d.json());
-						// console.log(reseponse);
 
+						const marker = new kakao.maps.Marker({
+							position: new kakao.maps.LatLng(selectedCafe.y, selectedCafe.x),
+						});
+						clusterer.addMarker(marker);
+						map.setCenter(
+							new kakao.maps.LatLng(selectedCafe.y, selectedCafe.x)
+						);
+
+						setMapLayOut(selectedCafe, map, marker);
+
+						const paramsObj = {
+							origin: `${lat}, ${lon}`,
+							destination: `${selectedCafe.y}, ${selectedCafe.x}`,
+							priority: "RECOMMEND",
+							alternatives: "true",
+							road_details: "true",
+							summary: "true",
+						} as any;
+						const queryStr = new URLSearchParams(paramsObj).toString();
 						const response = await fetch(
-							`/api/kakamapDetail?id=${selectedCafe[0].id}`
-						).then((d) => d.json());
-						console.log(response);
-						displayMarker(selectedCafe[0], map);
+							`//apis-navi.kakaomobility.com/v1/directions?${queryStr}`,
+							{
+								headers: {
+									"Content-Type": "application/json",
+									Authorization: `KakaoAK ${KAKAO_KEY}`,
+								},
+							}
+						);
+						const result = await response.json();
+						console.log(result);
+
+						const polyline = new kakao.maps.Polyline({
+							map: map,
+							path: [
+								new kakao.maps.LatLng(selectedCafe.y, selectedCafe.x),
+								new kakao.maps.LatLng(lat, lon),
+							],
+							strokeWeight: 8,
+							strokeColor: "#FF00FF",
+							strokeOpacity: 0.8,
+							strokeStyle: "solid",
+						});
 					}
-				}
-
-				// 지도에 마커를 표시하는 함수입니다
-				function displayMarker(place: any, map: any) {
-					// 마커를 생성하고 지도에 표시합니다
-					const marker = new kakao.maps.Marker({
-						map: map,
-						position: new kakao.maps.LatLng(place.y, place.x),
-					});
-
-					// 마커에 클릭이벤트를 등록합니다
-					// kakao.maps.event.addListener(marker, "click", function () {
-					// 	// 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-					// 	infowindow.setContent(
-					// 		'<div style="padding:5px;font-size:12px;">' +
-					// 			place.place_name +
-					// 			"</div>"
-					// 	);
-					// 	infowindow.open(map, marker);
-					// });
-				}
+				};
+				// 카테고리로 은행을 검색합니다
+				ps.categorySearch("CE7", placesSearch, {
+					useMapBounds: true,
+				});
 			});
 		}
-	};
+	}, []);
+
+	useEffect(() => {
+		console.log(1);
+		if (!kakaoLoad) {
+			console.log(2);
+			// setKaKaoMap();
+		}
+	}, []);
 
 	return (
-		<div>
+		<div className='relative'>
 			<Script
 				src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_KEY}&autoload=false&libraries=services,clusterer,drawing`}
 				onLoad={() => {
-					if ((window as any).kakao) {
+					if (window.kakao) {
 						kakao.maps.load(() => {
+							setKakaoLoad(true);
 							setKaKaoMap();
 						});
 					}
 				}}
 			/>
-			<div ref={mapEl} className='w-full h-full min-h-[500px]'></div>
+			{loading && (
+				<Skeleton className='w-full h-full min-h-[500px] absolute top-0 left-0 z-10' />
+			)}
+			<div
+				ref={mapEl}
+				className={`w-full h-full min-h-[500px] ${
+					loading ? "invisible" : "block"
+				}`}
+			></div>
 		</div>
 	);
 }
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -22,7 +22,7 @@ function MountainIcon({ className }: { className: string }) {
 export default function Navigation() {
 	return (
 		<header className='px-4 lg:px-6 h-14 flex items-center'>
-			<Link className='flex items-center justify-center' href='#'>
+			<Link className='flex items-center justify-center' href='/'>
 				<MountainIcon className='h-6 w-6' />
 				<span className='sr-only'>Acme Inc</span>
 			</Link>
@@ -35,9 +35,9 @@ export default function Navigation() {
 				</Link>
 				<Link
 					className='text-sm font-medium hover:underline underline-offset-4'
-					href='#'
+					href={"/user/signup"}
 				>
-					About
+					JOIN
 				</Link>
 				<Link
 					className='text-sm font-medium hover:underline underline-offset-4'
