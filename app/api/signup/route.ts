dc4f65c0a70b2ad97271fe2d9434f0e6add7da53
asd0905 <dydcodydco@daum.net>
2024-04-09 18:49:15 +0900

    feat: 회원가입 api 위치 및 로직 변경

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,21 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient({
 	log: ["query", "info", "warn", "error"],
 });
 
-export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse
-) {
-	if (req.method !== "POST") {
-		return res;
-	}
-
+export async function POST(req: Request) {
+	console.log(req);
 	try {
-		const { email, password, name } = req.body;
+		const requestBody = await req.json();
+		const { email, password, name } = requestBody;
 		const hashedPassword = bcrypt.hashSync(password, 8);
 
 		const user = await prisma.user.create({
@@ -26,8 +22,14 @@ export default async function handler(
 			},
 		});
 
-		res.status(201).json({ message: "User created", user });
+		return NextResponse.json({
+			message: "User created",
+			user,
+		});
+		// res.status(201).json({ message: "User created", user });
 	} catch (error) {
-		res.status(500).json({ message: "Internal server error" });
+		return NextResponse.json({
+			message: "Internal server error",
+		});
 	}
 }
--- app/user/signup/page.tsx
+++ app/user/signup/page.tsx
@@ -10,7 +10,7 @@ export default function SignUp() {
 	return (
 		<div className='flex flex-col min-h-[100dvh]'>
 			<main className='flex-1'>
-				<section className='w-full py-12 md:py-24 lg:py-32 xl:py-48'>
+				<section className='w-full py-8 pb-7 md:py-12 lg:py-20'>
 					<div className='container flex flex-col items-center justify-center space-y-4 text-center px-4 md:px-6'>
 						<div className='space-y-2'>
 							<h1 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl'>
